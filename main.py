a70562decdf919f0335f6db62aeac68cdaab143b
Your Name <you@example.com>
2025-04-22 23:17:34 -0700

    reads the text file

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 def main():
-    pass
+    print("================ BOOKBOT ================")
+
+    file_name = input("Enter file name: ")
+
+    print(f"Analyzing book for in readings/{file_name}...")
+
+    print("------------- WORD COUNT -------------")
+
+    num_line = 0
+    with open(f"readings/{file_name}", "r") as f:
+        for line in f:
+            print(line)
+
+        
+    
+        
+        
 
 if __name__== '__main__':
     main()
+
